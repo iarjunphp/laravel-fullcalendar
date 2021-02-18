1f79693539782380f7eedb05d389fda1d9559230
Arjun <arjunphp@gmail.com>
2021-02-18 20:00:25 +0530

    composer.json file updated

---
+++ src/Arjunphp/LaravelFullcalendar/IdentifiableEvent.php
@@ -0,0 +1,13 @@
+<?php namespace Arjunphp\LaravelFullcalendar;
+
+interface IdentifiableEvent extends Event
+{
+
+    /**
+     * Get the event's ID
+     *
+     * @return int|string|null
+     */
+    public function getId();
+
+}
